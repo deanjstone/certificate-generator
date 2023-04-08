51ced84f8dbc38dce44dac48fa139f82d90a67b0
deanojstone86 <50617844+deanojstone86@users.noreply.github.com>
2023-04-08 17:20:39 +1000

    Unique Number Generator from ChatGPT

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+// Get DOM elements
+const seedInput = document.getElementById("seed");
+const generateBtn = document.getElementById("generate-btn");
+const outputField = document.getElementById("output");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generateNumber);
+
 function generateNumber() {
-  const input = document.getElementById("input").value;
-  const epoch = Math.floor(Date.now() / 1000); // Convert milliseconds to seconds
-  const seed = input + epoch;
-  const hash = CryptoJS.SHA384(seed.toString()).toString();
-  const number = parseInt(hash.slice(0, 9), 16); // Take the first 9 hex digits and convert to decimal
-  const uniqueNumber = (number % 40000000) + 10000000; // Convert to 7 digits, starting with 1
-  document.getElementById("output").textContent = uniqueNumber;
+  // Get seed number from input field
+  const seed = seedInput.value;
+
+  // Get current epoch time in seconds
+  const epochTime = Math.floor(Date.now() / 1000);
+
+  // Concatenate seed and epoch time
+  const concatenated = `${seed}${epochTime}`;
+
+  // Generate a hash from concatenated string
+  const hash = hashCode(concatenated);
+
+  // Convert hash to a number between 6 and 9 digits
+  const min = 100000;
+  const max = 999999999;
+  const generatedNumber = Math.floor((hash % (max - min + 1)) + min);
+
+  // Set output field value
+  outputField.value = generatedNumber;
 }
 
-document
-  .getElementById("generate-btn")
-  .addEventListener("click", generateNumber);
+// Hash function
+function hashCode(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return hash;
+}
